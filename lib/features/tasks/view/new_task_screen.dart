9aba982b9becfca615baf35123650672a8d89692
toanlemanh <chen22112k3@gmail.com>
2025-05-12 18:41:12 +0700

    update task form screen

--- lib/features/tasks/view/new_task_screen.dart
+++ lib/features/tasks/view/new_task_screen.dart
@@ -1,9 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project/core/routing/app_routes.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../../core/widgets/task_form.dart';
+import '../model/task.dart';
+import '../service/task_service.dart';
+import '../viewmodel/task_store.dart';
+final task =
+  Task(1, 'eat breakfast', 'eat detail', DateTime.timestamp(), DateTime(2025,DateTime.december,1), DateTime(2025,DateTime.december,12) );
 
 class NewTaskScreen extends StatelessWidget{
+
+  final taskViewModel = TaskStore(taskService: TaskService());
+
   @override
   Widget build(BuildContext context) {
-    return const Text('form');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tasks'),
+        backgroundColor: Colors.deepPurple,
+        leading: IconButton(
+          onPressed: () => context.go(AppRoutes.home.path),
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.grey,
+          ),
+        ),
+      ),
+      body: task == null // length
+          ? const Center(child: Text('No tasks yet'))
+          : Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TaskForm(
+                // key: ValueKey(task.taskId.toString()),
+                // model: task,
+                // viewmodel: taskViewModel,
+              ),
+          ),
+          );
+        }
   }
 
-}
